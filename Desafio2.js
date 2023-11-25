aa9ae587b0eda51b933a99ad9539c39a085aadd6
Arthur Ferreira <arthurfernandesferreira@hotmail.com>
2023-11-25 13:00:49 -0300

    Atualização do Projeto

--- Desafio2.js
+++ Desafio2.js
@@ -21,8 +21,7 @@ const jwtSecret = process.env.JWT_SECRET;
 app.use(bodyParser.json());
 
 let authToken = '';
-
-let token;
+let userId = '';
 
 const authenticateToken = async (req, res, next) => {
     const tokenHeader = req.headers['authorization'];
@@ -39,7 +38,8 @@ const authenticateToken = async (req, res, next) => {
 
     try {
         const decoded = await jwt.verify(token, process.env.JWT_SECRET);
-        req.user = { userId: decoded.userId };
+        userId = decoded.usuarioId;  // Atribui o userId à variável global
+        req.user = { userId: decoded.usuarioId };
         next();
     } catch (err) {
         if (err.name === 'TokenExpiredError') {
@@ -49,20 +49,137 @@ const authenticateToken = async (req, res, next) => {
     }
 };
 
+function realizarSignIn() {
+    app.post('/signin', async (req, res) => {
+        const { email, senha } = req.body;
+
+        try {
+            const [usuario] = await db.execute('SELECT * FROM usuarios WHERE email = ? AND senha = ?', [email, senha]);
+
+            if (usuario.length === 0) {
+                return res.status(401).json({ error: 'Usuário e/ou senha inválidos.' });
+            }
+            const userId = usuario[0].id;
+            await db.execute('UPDATE usuarios SET ultimo_login = ?, data_atualizacao = ?, token = ? WHERE id = ?',
+                [new Date(), new Date(), jwt.sign({ usuarioId: usuario[0].id }, process.env.JWT_SECRET, { expiresIn: '30m' }), usuario[0].id]);
+
+            res.json({
+                id: usuario[0].id,
+                data_criacao: usuario[0].data_criacao,
+                data_atualizacao: usuario[0].data_atualizacao,
+                ultimo_login: new Date(),
+                token: jwt.sign({ usuarioId: usuario[0].id }, process.env.JWT_SECRET, { expiresIn: '30m' }),
+            });
+        } catch (error) {
+            console.error('Erro ao autenticar usuário:', error);
+            res.status(500).json({ error: 'Erro interno do servidor.' });
+        }
+    });
+    rl.question('Digite seu e-mail: ', (email) => {
+        rl.question('Digite sua senha: ', async (senha) => {
+            try {
+                const resposta = await axios.post('http://localhost:3000/signin', {
+                    email,
+                    senha,
+                });
+                console.log('Resposta da API:', resposta.data);
+                if (resposta.data.token) {
+                    authToken = resposta.data.token;
+                    userId = resposta.data.id;
+                } else {
+                    console.error('Token não encontrado na resposta da API.');
+                }
+            } catch (erro) {
+                console.error('Erro na chamada da API:', erro.message);
+            }
+            mostrarOpcoes(); // Continua mostrando as opções
+        });
+    });
+}
+
  
 function realizarSignUp(nome, email, senha, telefoneNumero, telefoneDDD) {
-    const telefone = [{ numero: telefoneNumero, ddd: telefoneDDD }];
-    
-    return axios.post('http://localhost:3000/signup', { nome, email, senha, telefone })
-      .then((resposta) => {
-          authToken = resposta.data.token;
-          console.log('Cadastro bem-sucedido!');
-          mostrarOpcoes(); // Chama a próxima opção automaticamente após o cadastro
-      })
-      .catch((erro) => {
-        console.error('Erro na chamada da API:', erro.message);
-      });
+    app.post('/signup', async (req, res) => {
+        const { nome, email, senha, telefone } = req.body;
+
+        try {
+            const [existeEmail] = await db.execute('SELECT * FROM usuarios WHERE email = ?', [email]);
+
+            if (existeEmail.length > 0) {
+                return res.status(400).json({ error: 'E-mail já existente.' });
+            }
+
+            const novoUsuario = {
+                id: uuid.v4(),
+                nome,
+                email,
+                senha,
+                telefone,
+                data_criacao: new Date(),
+                data_atualizacao: new Date(),
+                ultimo_login: null,
+                token: null,
+            };
+
+            await db.execute('INSERT INTO usuarios (id, nome, email, senha, telefone, data_criacao, data_atualizacao) VALUES (?, ?, ?, ?, ?, ?, ?)',
+                [novoUsuario.id, novoUsuario.nome, novoUsuario.email, novoUsuario.senha, JSON.stringify(novoUsuario.telefone), novoUsuario.data_criacao, novoUsuario.data_atualizacao]);
+
+            res.status(201).json({
+                id: novoUsuario.id,
+                data_criacao: novoUsuario.data_criacao,
+                data_atualizacao: novoUsuario.data_atualizacao,
+                ultimo_login: novoUsuario.ultimo_login,
+                token: novoUsuario.token,
+            });
+        } catch (error) {
+            console.error('Erro ao cadastrar usuário:', error);
+            res.status(500).json({ error: 'Erro interno do servidor.' });
+        }
+    });
+    rl.question('Digite seu nome: ', (nome) => {
+        rl.question('Digite seu e-mail: ', (email) => {
+            rl.question('Digite sua senha: ', (senha) => {
+                rl.question('Digite o número de telefone: ', (telefoneNumero) => {
+                    rl.question('Digite o DDD do telefone: ', async (telefoneDDD) => {
+                        const telefone = [{ numero: telefoneNumero, ddd: telefoneDDD }];
+                        return axios.post('http://localhost:3000/signup', { nome, email, senha, telefone })
+                        .then((resposta) => {
+                            authToken = resposta.data.token;
+                            console.log('Cadastro bem-sucedido!');
+                            mostrarOpcoes(); // Chama a próxima opção automaticamente após o cadastro
+                        })
+                        .catch((erro) => {
+                            console.error('Erro na chamada da API:', erro.message);
+                        });
+                    });
+                });
+            });
+        });
+    });
 }
+
+function RecuperaDados() {
+    app.get('/user', authenticateToken, async (req, res) => {
+        // Lógica para recuperar informações do usuário
+        try {
+            const [user] = await db.execute('SELECT * FROM usuarios WHERE id = ?', [userId]);
+            if (user.length === 0) {
+                return res.status(404).json({ error: 'Usuário não encontrado.' });
+            }
+            res.json({
+                id: user[0].id,
+                nome: user[0].nome,
+                email: user[0].email,
+                telefone: user[0].telefone,
+            });
+        } catch (error) {
+            console.error('Erro ao recuperar informações do usuário:', error);
+            res.status(500).json({ error: 'Erro interno do servidor.' });
+        }
+    });
+    mostrarOpcoes();
+}
+
   
 function mostrarOpcoes() {
     console.log('Escolha uma opção:');
@@ -71,133 +188,26 @@ function mostrarOpcoes() {
     console.log('3. Recuperar Dados');
     console.log('4. Sair');
   
-    rl.question('Digite o número da opção desejada: ', (opcao) => {
+    rl.question('Digite o número da opção desejada: ', async (opcao) => {
         switch (opcao) {
             case '1':
-                app.post('/signin', async (req, res) => {
-                    const { email, senha } = req.body;
-        
-                    try {
-                        const [usuario] = await db.execute('SELECT * FROM usuarios WHERE email = ? AND senha = ?', [email, senha]);
-            
-                        if (usuario.length === 0) {
-                            return res.status(401).json({ error: 'Usuário e/ou senha inválidos.' });
-                        }
-                        const userId = usuario[0].id;
-                        await db.execute('UPDATE usuarios SET ultimo_login = ?, data_atualizacao = ?, token = ? WHERE id = ?',
-                            [new Date(), new Date(), jwt.sign({ usuarioId: usuario[0].id }, process.env.JWT_SECRET, { expiresIn: '30m' }), usuario[0].id]);
-            
-                        res.json({
-                            id: usuario[0].id,
-                            data_criacao: usuario[0].data_criacao,
-                            data_atualizacao: usuario[0].data_atualizacao,
-                            ultimo_login: new Date(),
-                            token: jwt.sign({ usuarioId: usuario[0].id }, process.env.JWT_SECRET, { expiresIn: '30m' }),
-                        });
-                    } catch (error) {
-                        console.error('Erro ao autenticar usuário:', error);
-                        res.status(500).json({ error: 'Erro interno do servidor.' });
-                    }
-                });
-                rl.question('Digite seu e-mail: ', (email) => {
-                    rl.question('Digite sua senha: ', async (senha) => {
-                        try {
-                            const resposta = await axios.post('http://localhost:3000/signin', {
-                                email,
-                                senha,
-                            });
-                            console.log('Resposta da API:', resposta.data);
-                            if (resposta.data.token) {
-                                authToken = resposta.data.token;
-                                userId = resposta.data.id;
-                            } else {
-                                console.error('Token não encontrado na resposta da API.');
-                            }
-                        } catch (erro) {
-                            console.error('Erro na chamada da API:', erro.message);
-                        }
-                        mostrarOpcoes(); // Continua mostrando as opções
-                    });
-                });
+                realizarSignIn();
                 break;
-  
             case '2':
-                app.post('/signup', async (req, res) => {
-                    const { nome, email, senha, telefone } = req.body;
-            
-                    try {
-                        const [existeEmail] = await db.execute('SELECT * FROM usuarios WHERE email = ?', [email]);
-            
-                        if (existeEmail.length > 0) {
-                            return res.status(400).json({ error: 'E-mail já existente.' });
-                        }
-            
-                        const novoUsuario = {
-                            id: uuid.v4(),
-                            nome,
-                            email,
-                            senha,
-                            telefone,
-                            data_criacao: new Date(),
-                            data_atualizacao: new Date(),
-                            ultimo_login: null,
-                            token: null,
-                        };
-            
-                        await db.execute('INSERT INTO usuarios (id, nome, email, senha, telefone, data_criacao, data_atualizacao) VALUES (?, ?, ?, ?, ?, ?, ?)',
-                            [novoUsuario.id, novoUsuario.nome, novoUsuario.email, novoUsuario.senha, JSON.stringify(novoUsuario.telefone), novoUsuario.data_criacao, novoUsuario.data_atualizacao]);
-            
-                        res.status(201).json({
-                            id: novoUsuario.id,
-                            data_criacao: novoUsuario.data_criacao,
-                            data_atualizacao: novoUsuario.data_atualizacao,
-                            ultimo_login: novoUsuario.ultimo_login,
-                            token: novoUsuario.token,
-                        });
-                    } catch (error) {
-                        console.error('Erro ao cadastrar usuário:', error);
-                        res.status(500).json({ error: 'Erro interno do servidor.' });
-                    }
-                });
-                rl.question('Digite seu nome: ', (nome) => {
-                    rl.question('Digite seu e-mail: ', (email) => {
-                        rl.question('Digite sua senha: ', (senha) => {
-                            rl.question('Digite o número de telefone: ', (telefoneNumero) => {
-                                rl.question('Digite o DDD do telefone: ', async (telefoneDDD) => {
-                                    await realizarSignUp(nome, email, senha, telefoneNumero, telefoneDDD);
-                                    // A lógica de mostrar as opções agora é chamada dentro de realizarSignUp
-                                });
-                            });
-                        });
-                    });
-                });
-              
+                realizarSignUp();
                 break;
-  
             case '3':
-                app.get('/user', authenticateToken, async (req, res) => {
-                    // Apenas usuários autenticados podem acessar esta rota
-                    const usuarioId = req.usuario.usuarioId;
-                
-                    try {
-                        // Consulta as informações do usuário usando o ID extraído do token
-                        const [usuario] = await db.execute('SELECT * FROM usuarios WHERE id = ?', [usuarioId]);
-                
-                        if (usuario.length === 0) {
-                            return res.status(404).json({ error: 'Usuário não encontrado.' });
-                        }
-                
-                        res.json({
-                            id: usuario[0].id,
-                            nome: usuario[0].nome,
-                            email: usuario[0].email,
-                            telefone: usuario[0].telefone,
-                        });
-                    } catch (error) {
-                        console.error('Erro ao recuperar informações do usuário:', error);
-                        res.status(500).json({ error: 'Erro interno do servidor.' });
-                    }
-                });
+                try {
+                    const resposta = await axios.get('http://localhost:3000/user', {
+                        headers: {
+                            Authorization: `Bearer ${authToken}`,
+                        },
+                    });
+                    console.log('Dados do usuário:', resposta.data);
+                } catch (erro) {
+                    console.error('Erro na chamada da API:', erro.message);
+                }
+                RecuperaDados();
             break;
         case '4':
           console.log('Saindo...');
