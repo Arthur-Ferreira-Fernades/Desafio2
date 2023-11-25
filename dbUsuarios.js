f92a08c131ce6bec70ed33174ee4ff91ced6c0fd
Arthur Ferreira <arthurfernandesferreira@hotmail.com>
2023-11-25 14:22:24 -0300

    Primeiro commit

--- dbUsuarios.js
+++ dbUsuarios.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'dbdesafio2',
+    host: process.env.JAWSDB_HOST,
+    user: process.env.JAWSDB_USER,
+    password: process.env.JAWSDB_PASSWORD,
+    database: process.env.JAWSDB_DATABASE,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
